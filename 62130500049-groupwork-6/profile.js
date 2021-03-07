cd2a4733cf1d1cccb9aec85c18c26eaab07bf8fe
Patiphan <patiphan.new@mail.kmutt.ac.th>
2021-03-07 20:40:50 +0700

    submit v1.0.0

--- 62130500049-groupwork-6/profile.js
+++ 62130500049-groupwork-6/profile.js
@@ -5,29 +5,10 @@ const constraints = {
     lastname: {
         presence: true,
     },
-    age: {
-        presence: true,
-        numericality: {
-            lessThan: 150
-        }
-    },
-    gender: {
-        presence: true,
-    },
     email: {
         presence: true,
         email: true
     },
-    phone: {
-        presence: true,
-        length: {
-            minimum: 10,
-            message: "must be at least 10 digits"
-        },
-    },
-    chosensubject: {
-        presence: true
-    }
 }
 
 const app = Vue.createApp({
@@ -35,14 +16,7 @@ const app = Vue.createApp({
         return {
             firstname: null,
             lastname: null,
-            age: null,
-            gender: null,
             email: null,
-            phone: null,
-            chosensubject: null,
-            subject_lists: [{ subject_id: 1, subject_name: 'Intro to Vue 3' },
-            { subject_id: 2, subject_name: 'Vue 3 Forms' },
-            { subject_id: 3, subject_name: 'Vue 3 Reactivity' }],
             errors: null,
         }
     },
@@ -51,11 +25,7 @@ const app = Vue.createApp({
             this.errors = validate({
                 firstname: this.firstname,
                 lastname: this.lastname,
-                age: this.age,
-                gender: this.gender,
                 email: this.email,
-                phone: this.phone,
-                chosensubject: this.chosensubject
             },
                 constraints)
             if (!this.errors) {
